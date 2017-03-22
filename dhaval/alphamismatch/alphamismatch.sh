6f9cce5b4563d5ed656cf23f57e53eb95617f39f
drdhaval2785 <drdhaval2785@gmail.com>
2017-03-22 10:17:37 +0530

    alphamismatch regenerated

--- dhaval/alphamismatch/alphamismatch.sh
+++ dhaval/alphamismatch/alphamismatch.sh
@@ -1,4 +1,4 @@
-python alphamismatch.py
+python alphamismatch.py > log.txt
 list=(ACC CAE AE AP90 AP BEN BHS BOP BOR BUR CCS GRA GST IEG INM KRM MCI MD MW72 MW MWE PD PE PGN PUI PWG PW SCH SHS SKD SNP STC VCP VEI WIL YAT)
 for Val in "${list[@]}"
 do
