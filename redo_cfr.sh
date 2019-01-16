701ac5231c1360f873d0390df3534bf2d2f919d3
funderburkjim <funderburkjim@gmail.com>
2019-01-15 20:30:16 -0500

    corrections to mw

--- redo_cfr.sh
+++ redo_cfr.sh
@@ -1,6 +1,6 @@
 echo "This script needs to be run with GitBash on windows OS"
 echo "download new cfr.tsv from Cologne"
-curl -o cfr.tsv http://www.sanskrit-lexicon.uni-koeln.de/php/correction_response/cfr.tsv
+curl -o cfr.tsv https://www.sanskrit-lexicon.uni-koeln.de/php/correction_response/cfr.tsv
 echo "regenerate correctionform.txt"
 python cfr_adj.py cfr.tsv correctionform.txt
 echo "redo_cfr.sh is finished"
