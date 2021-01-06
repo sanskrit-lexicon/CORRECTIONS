e89870bc7d928ebe012449324eed73e52e052a16
drdhaval2785 <drdhaval2785@gmail.com>
2021-01-06 21:33:18 +0530

    sh updated

--- abnormending/abnormending.sh
+++ abnormending/abnormending.sh
@@ -2,7 +2,7 @@ len=(3)
 for value in "${len[@]}"
 do
 	echo "Running abnormending.py"
-	python abnormending.py $value
+	python abnormending.py $value 10
 	echo "Linking the webpage and PDFs"
 	php link.php abnorm_$value.txt abnorm_$value.html Suspect_word_ends 178
 done
