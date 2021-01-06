444fe2decee95b46d885a8c2d6f1026547cf1a67
drdhaval2785 <drdhaval2785@gmail.com>
2021-01-06 21:31:38 +0530

    abnorm_3.txt based on 10 threshold

--- abnormending/abnormending.py
+++ abnormending/abnormending.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """ abnormending.py
 
-Usage - python3 abnormending.py lettersAtEnd
-e.g. python3 abnormending.py 3
+Usage - python3 abnormending.py lettersAtEnd threshold
+e.g. python3 abnormending.py 3 10
 
-To generate words ending with abnormal endings (maybe less than 50 headwords).
+This will generate words having 3-letter endings occurring in less than 10 headwords.
 """
 from __future__ import print_function
 import sys
-import re
 import codecs
 import os
 import datetime
@@ -42,44 +41,14 @@ def abnormending(n):
         res[end].append(datum)
     res1 = OrderedDict(sorted(res.items(), key=lambda t: t[0]))
     for (key, value) in res1.items():
-        if len(value) < 50:
+        if len(value) < 10:
             for val in value:
                 # Only one dictionary has that thing
                 if ',' not in val:
                     print(key, val)
                     fout.write(val + '\n')
-    exit(0)
-    """
-    for (end, datum) in output:
-        if end not in endings:
-            endings.append(end)
-            print(end)
-    print(len(endings))
-    withcounter = []
-    for ends in endings:
-        counter = 0
-        for datum in data:
-            if re.search(ends + ':', datum):
-                counter += 1
-        if counter < 50:
-            withcounter.append((counter, ends))
-            print(counter, ends)
-        else:
-            print(ends + 'has more than 50 words')
-    withcounter = sorted(withcounter, key=lambda x: x[0])
-    print("Culled out and sorted abnormal endings")
-
-    print("Writing suspect entries to abnorm.txt.")
-    print("This would take some time.")
-    nochange = codecs.open('../nochange/nochange1.txt', 'r', 'utf-8')
-    noc = nochange.readlines()
-    noc = triming(noc)
-    for (count, end) in withcounter:
-        for datum in data:
-            if re.search(end + ':[^,]*$', datum) and datum not in noc:
-                fout.write(datum + "\n")
     fout.close()
-    """
+
 
 if __name__ == "__main__":
     n = sys.argv[1]
